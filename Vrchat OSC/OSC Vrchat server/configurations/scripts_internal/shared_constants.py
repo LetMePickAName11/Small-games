6893d25059f79cb1abaa00b464fc355ca0260ba2
LetMePickAName11 <lucas-van@hotmail.dk>
2024-05-08 20:31:25 +0200

    Get rid of filthy snakes

--- Vrchat OSC/OSC Vrchat server/configurations/scripts_internal/shared_constants.py
+++
@@ -1,4 +0,0 @@
-TEMPLATE_DIRECTORY = 'templates/'
-USER_INPUT_DIRECTORY = 'user_defined_data/'
-OUTPUT_INTERNAL_DIRECTORY = 'auto_generated_files_internal/'
-OUTPUT_EXTERNAL_DIRECTORY = 'auto_generated_files_external/'
